efcd746999b1ac77fcd55dca2be122b47ceaeb00
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-10-08 12:04:54 +0100

    Adds #balance to initialize; renames Transaction class

--- lib/account.rb
+++ lib/account.rb
@@ -1,13 +1,11 @@
-class Transaction
-  attr_reader :transactions
+class Account
+  attr_reader :transactions, :balance
 
   def initialize
     @transactions = []
+    @balance = 0
   end
 
   def withdraw(amount)
-    
   end
-
-
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,19 +1,21 @@
-require "transaction"
+require "account"
 
-describe Transaction do
+describe Account do
   subject { described_class.new }
 
   describe "#initialize" do
     it "initializes with an array of transactions" do
-      transaction = Transaction.new
-      expect(transaction.transactions).to be_an(Array)
+      expect(subject.transactions).to be_an(Array)
+    end
+
+    it "initializes with an opening balance of 0" do
+      expect(subject.balance).to eq(0)
     end
   end
 
   describe "withdraw" do
     it "takes a param to withdraw" do
-      expect{ subject.withdraw(100) }.not_to raise_error
+      expect { subject.withdraw(100) }.not_to raise_error
     end
   end
-
 end
