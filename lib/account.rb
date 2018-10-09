59ee0af7a63dc613ef1cdfefbc5f092ddf01a08a
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-10-09 08:11:23 +0100

    Adds date to transaction records

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,5 @@
+require "date"
+
 class Account
   attr_reader :transactions, :balance
 
@@ -6,14 +8,20 @@ class Account
     @balance = 0.00
   end
 
-  def withdraw(amount)
+  def withdraw(date, amount)
+    date = date_formatter(date)
     @balance = @balance - amount.abs
-    @transactions << -amount.abs
+    @transactions << [date, -(amount.abs)]
   end
 
-  def deposit(amount)
+  def deposit(date, amount)
+    date = date_formatter(date)
     @balance = @balance + amount.abs
-    @transactions << amount.abs
+    @transactions << [date, amount.abs]
   end
   ## NOTE: Currently keeping the #.abs method on #deposit in case deposit / withdraw are renamed to credit / debit, which can be confused easily depending on whether you're considering this from the customer of bank's point of view.
+
+  def date_formatter(date)
+    Date.strptime(date).strftime("%d/%m/%Y")
+  end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,7 +1,8 @@
 require "account"
 
 describe Account do
-  subject { described_class.new }
+  let(:date) { "11-11-2011" }
+  let(:subject) { described_class.new }
 
   describe "#initialize" do
     it "initializes with an array of transactions" do
@@ -15,45 +16,46 @@ describe Account do
 
   describe "#withdraw" do
     it "reduces the balance by the given amount" do
-      subject.withdraw(-100.00)
-      expect(subject.balance).to eq(-100.00)
-    end
+    subject.withdraw(date, -100.00)
+    expect(subject.balance).to eq(-100.00)
+  end
 
     it "reduces balance by correct value when given positive number" do
-      expect { subject.withdraw(100.00) }.to change { subject.balance }.by(-100.00)
+      expect { subject.withdraw(date, 100.00) }.to change { subject.balance }.by(-100.00)
       expect(subject.balance).to eq(-100.00)
     end
 
     it "reduces balance by correct value when given negative number" do
-      expect { subject.withdraw(-100.00) }.to change { subject.balance }.by(-100.00)
+      expect { subject.withdraw(date, -100.00) }.to change { subject.balance }.by(-100.00)
       expect(subject.balance).to eq(-100.00)
     end
 
     it "adds a withdrawl amount as a negative float to transactions array" do
-      subject.withdraw(100.00)
-      expect(subject.transactions).to include(-100.00)
+      subject.withdraw(date, 100.00)
+      expect(subject.transactions[0]).to include(-100.00)
     end
   end
 
+
   describe "#deposit" do
     it "increases the balance by the given amount" do
-      subject.deposit(100.00)
+      subject.deposit(date, 100.00)
       expect(subject.balance).to eq(100.00)
     end
 
     it "increases balance by correct value when given positive number" do
-      expect { subject.deposit(100.00) }.to change { subject.balance }.by(100.00)
+      expect { subject.deposit(date, 100.00) }.to change { subject.balance }.by(100.00)
       expect(subject.balance).to eq(100.00)
     end
 
     it "increases balance by correct value when given negative number" do
-      expect { subject.deposit(-100.00) }.to change { subject.balance }.by(100.00)
+      expect { subject.deposit(date, -100.00) }.to change { subject.balance }.by(100.00)
       expect(subject.balance).to eq(100.00)
     end
 
     it "adds a deposited amount as a positive float to transactions array" do
-      subject.deposit(500.00)
-      expect(subject.transactions).to include(500.00)
+      subject.deposit(date, 500.00)
+      expect(subject.transactions[0]).to include(500.00)
     end
   end
 end
