8946680f3141881fd7b6efddc609b47e93a370eb
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-10-09 10:45:01 +0100

    Adds #initialize params for Transaction

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,2 +1,10 @@
 class Transaction
+  attr_reader :date, :debit, :credit, :balance
+
+  def initialize(date, debit, credit, balance)
+    @date = date
+    @debit = debit
+    @credit = credit
+    @balance = balance
+  end
 end
