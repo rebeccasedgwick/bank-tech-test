e3c0ce2f4aa4b37a8e238cf197c7df83d15038ef
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-10-09 10:46:01 +0100

    Refactors to match credit/debit order as per README instructions

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,10 +1,10 @@
 class Transaction
   attr_reader :date, :debit, :credit, :balance
 
-  def initialize(date, debit, credit, balance)
+  def initialize(date, credit, debit, balance)
     @date = date
-    @debit = debit
     @credit = credit
+    @debit = debit
     @balance = balance
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -4,10 +4,10 @@ describe Transaction do
   subject { described_class.new("11-01-2001", "500", "", "500") }
 
   describe "#initialize" do
-    it "returns the date, debit, credit, and balance of a transaction" do
+    it "returns the date, credit, debit, and balance of a transaction" do
       expect(subject.date).to eq("11-01-2001")
-      expect(subject.debit).to eq("500")
-      expect(subject.credit).to eq("")
+      expect(subject.credit).to eq("500")
+      expect(subject.debit).to eq("")
       expect(subject.balance).to eq("500")
     end
   end
