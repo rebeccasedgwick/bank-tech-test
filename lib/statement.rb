c5abcbf8eb83b019a9bd740fce74c2ec5bb96caa
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-10-11 10:35:50 +0100

    Basic test for returning statment on #show

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,4 +1,9 @@
 class Statement
   def initialize(account = Account)
+    @account = account
+  end
+
+  def show
+    puts @account.transactions
   end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -2,10 +2,25 @@ require "statement"
 
 describe Statement do
   let(:account_class_double) { double }
+  let(:account_class_transactions_double) { double }
+  let(:subject) { described_class.new(account_class_double) }
 
   describe "#initialize" do
     it "initializes with an account" do
       expect { Statement.new(account_class_double) }.to_not raise_error
+    end
+  end
+
+  describe "#show" do
+    context "dislaying the account's transactions in statment format"
+    it "shows the column headers" do
+      tr_dbl1 = double(date: Date.new(2011, 1, 1), debit: 50.0, credit: 0.0, balance: 250.0)
+      tr_dbl2 = double(date: Date.new(2011, 2, 1), debit: 0.0, credit: 500.0, balance: 750.0)
+
+      account_class_transactions_double = [tr_dbl1, tr_dbl2]
+      expect(account_class_double).to receive(:transactions).and_return(account_class_transactions_double)
+
+      expect { subject.show }.to output("#{tr_dbl1}\n#{tr_dbl2}\n").to_stdout
     end
   end
 end
