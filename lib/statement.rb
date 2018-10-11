7adf8f8641e14f7ab8d96272419e832c45321ed5
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-10-11 11:11:38 +0100

    Adds #show method to Statement

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,9 +1,28 @@
 class Statement
+  HEADERS = "date       || credit      || debit       || balance    "
+
   def initialize(account = Account)
     @account = account
   end
 
   def show
-    puts @account.transactions
+    puts HEADERS
+    @account.transactions.reverse_each do |transaction|
+      puts [
+        prettify_date(transaction.date).ljust(10, " "),
+        format_number(transaction.credit).rjust(12, " "),
+        format_number(transaction.debit).rjust(12, " "),
+        format_number(transaction.balance).rjust(12, " ")
+      ].join(" ||")
+    end
+  end
+
+  private
+  def prettify_date(date)
+    date.strftime("%d/%m/%Y")
+  end
+
+  def format_number(number)
+    "%.2f" % number.abs
   end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -12,15 +12,17 @@ describe Statement do
   end
 
   describe "#show" do
-    context "dislaying the account's transactions in statment format"
-    it "shows the column headers" do
-      tr_dbl1 = double(date: Date.new(2011, 1, 1), debit: 50.0, credit: 0.0, balance: 250.0)
-      tr_dbl2 = double(date: Date.new(2011, 2, 1), debit: 0.0, credit: 500.0, balance: 750.0)
+    it "shows the column headers & transactions in reverse chronological order" do
+      tr_dbl1 = double(date: Date.new(2011, 1, 1), debit: 0.0, credit: 500.0, balance: 500.0)
+      tr_dbl2 = double(date: Date.new(2011, 2, 1), debit: 250.0, credit: 0.0, balance: 250.0)
 
       account_class_transactions_double = [tr_dbl1, tr_dbl2]
+
       expect(account_class_double).to receive(:transactions).and_return(account_class_transactions_double)
 
-      expect { subject.show }.to output("#{tr_dbl1}\n#{tr_dbl2}\n").to_stdout
+      expected_string = "date       || credit      || debit       || balance    \n01/02/2011 ||        0.00 ||      250.00 ||      250.00\n01/01/2011 ||      500.00 ||        0.00 ||      500.00\n"
+
+      expect { subject.show }.to output(expected_string).to_stdout
     end
   end
 end
